914db8e5fcc37b9541d0651c14cba968538bc0de
lazarov <lazarov@gazeta.pl>
2015-06-02 17:18:26 +0200

    drugi

--- js/loader.js
+++ js/loader.js
@@ -1,2 +1,7 @@
-$(document).ready(function () {
-	
+$(document).ready(function() {
+	$('div.menu ul li a').click(function() {
+			var page = $(this).attr('href');
+			$('#content').load('sub/' + page + '.html');
+			return false;
+	});
+});
